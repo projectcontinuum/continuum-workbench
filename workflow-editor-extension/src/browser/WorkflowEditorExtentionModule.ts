c1077ad566301aa6348e191213241d142fc7fd0d
projectcontinuum <projectdevcontinuum@gmail.com>
2026-03-21 09:33:21 +0530

    CONTNM-53: Remove workflow-status widget

--- workflow-editor-extension/src/browser/WorkflowEditorExtentionModule.ts
+++ workflow-editor-extension/src/browser/WorkflowEditorExtentionModule.ts
@@ -7,10 +7,7 @@ import NodeRepoWidgetFactory from './widgets/node-repo/NodeRepoWidgetFactory';
 import ContinuumThemeService from './theme/ContinuumThemeService';
 import { ContinuumFrontendApplicationContribution } from './contribution/ContinuumFrontendApplicationContribution';
 import ContinuumFileTreeLabelProviderContribution from './contribution/ContinuumLabelProviderContribution';
-import WorkflowStatusWidgetFactory from './widgets/workflow-status/WorkflowStatusWidgetFactory';
-import WorkflowStatusViewContribution from './widgets/workflow-status/WorkflowStatusViewContribution';
 import { NodeRepoViewContribution } from './widgets/node-repo/NodeRepoViewContribution';
-import { WorkflowStatusWidget } from './widgets/workflow-status/WorkflowStatusWidget';
 import NodeRepoWidget from './widgets/node-repo/NodeRepoWidget';
 import CreateNewWorkflowCommand from './command/CreateNewWorkflowCommand';
 import { CommandContribution, MenuContribution } from '@theia/core';
@@ -73,10 +70,10 @@ export default new ContainerModule((bind) => {
     bindViewContribution(bind, NodeRepoViewContribution);
 
     // WorkflowStatus widget
-    bind(WorkflowStatusWidget).toSelf().inSingletonScope();
-    bind(WorkflowStatusWidgetFactory).toSelf().inSingletonScope();
-    bind(WidgetFactory).toService(WorkflowStatusWidgetFactory);
-    bindViewContribution(bind, WorkflowStatusViewContribution);
+    // bind(WorkflowStatusWidget).toSelf().inSingletonScope();
+    // bind(WorkflowStatusWidgetFactory).toSelf().inSingletonScope();
+    // bind(WidgetFactory).toService(WorkflowStatusWidgetFactory);
+    // bindViewContribution(bind, WorkflowStatusViewContribution);
 
     // NodeExplorer widget - using Theia TreeWidget
     bind(NodeExplorerService).toSelf().inSingletonScope();
